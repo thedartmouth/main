8ce457e0c358f392b6cc363bd1ee6dd4b6e12509
Alex Gerstein <gerstein.alex@gmail.com>
2014-04-10 12:57:10 -0400

    Made donation a link, not button.

--- includes/aboutnav.php
+++ includes/aboutnav.php
@@ -3,13 +3,7 @@
     <ul class="about-nav">
         <li><a href="/about">About The Dartmouth</a></li>
         <li><a href="/advertise">Advertise</a></li>
-        <li id="donate-form">
-            <form name="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-            <input type="hidden" name="cmd" value="_s-xclick">
-            <input type="hidden" name="hosted_button_id" value="RFHMXHM3RB43G">
-            <a href="#" onclick="document.donate.submit();">Donate</a>
-            </form>
-        </li>
+        <li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X3DCF7HWRVKAY" target="_blank">Donate</a></li>
         <li> <a href="/subscribe">Subscribe</a></li>
         <li><a href="/policies">Policies</a></li>
     </ul>
--- includes/topnav.php
+++ includes/topnav.php
@@ -5,13 +5,6 @@
 		<li><a href="/advertise">Advertise</a></li>
 		<li><a href="/subscribe">Subscribe</a></li>
 		<li><a href="http://thedartmouth.us4.list-manage2.com/subscribe?u=946d04da6b4c5e2ccab9bcf78&id=06f42def36">eSubscribe</a></li>
-
-		<li id="donate-form">
-			<form name="donate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
-			<input type="hidden" name="cmd" value="_s-xclick">
-			<input type="hidden" name="hosted_button_id" value="RFHMXHM3RB43G">
-			<a href="#" onclick="document.donate.submit();">Donate</a>
-			</form>
-		</li>
+		<li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X3DCF7HWRVKAY" target="_blank">Donate</a></li>
 	</ul>
 </div>
