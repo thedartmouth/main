df1e7d95f2686308f9b2c9aed1b2bcc7d59fbe82
Alex Gerstein <gerstein.alex@gmail.com>
2014-05-12 23:23:22 -0400

    Fixed index page.

---
+++ green_key/index.php
@@ -0,0 +1,28 @@
+<?php ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><meta name="blitz" content="mu-888cf5c6-e0ed7357-64cc0734-6f33dfe6"/>
+  <title>The Dartmouth - Green Key Special</title>
+  <?php include("../includes/htmlhead.php");
+
+    include("../included.php");
+
+    ?>
+    <link href="includes/green_key.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+    <?php include("includes/nav.php"); ?>
+
+    <div class="row-fluid">
+      <div class="span12">
+      </div>
+    </div>
+
+  </body>
+
+
+</html>
