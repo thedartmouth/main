2820ae1b0195453904972c6d5dddb6df6febeced
Robbie <rnhaus@gmail.com>
2015-01-29 03:44:42 -0500

    more mdf navbar stuff

---
+++ wp-content/themes/dartmouth/page-mdf-tweets.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Dartmouth
+ * @since Dartmouth 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+	<div class="special-issue container">
+			<div class="text-center special-issue-header">Moving Dartmouth Forward</div>
+			<nav role="navigation" class="navigation site-navigation extra-navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'extra', 'menu_class' => 'nav-menu col-md-12' ) ); ?>
+			</nav>
+		</div>
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+				endwhile;
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_footer();
