43517e64d781478bb34bdf8fc0a0dcaa986cf647
Robbie <rnhaus@gmail.com>
2015-03-06 03:50:44 -0500

    Mirror Ideal Journey page

---
+++ wp-content/themes/dartmouth/page-mirror-ideal-journey.php
@@ -0,0 +1,170 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+  <style>
+  body {
+    background-color: rgb(249, 247, 248);
+  }
+  .backbutton {
+    top: 10px;
+    left: 10px;
+    color: white;
+    padding: 20px;
+  }
+  .intro {
+    width: 100%;
+    text-align: center;
+    bottom:0;
+    height:100vh;
+  }
+
+  .header {
+    font-family: 'Lora', serif;
+    font-size: 3em;
+    z-index: 200;
+    left: 0;
+    right: 0;
+    width: 100%;
+  }
+
+  .blurb {
+    font-family: 'Lora', serif;
+    font-size: 2em;
+    padding: 20px;
+
+  }
+
+  #imgholder {
+    width: 407px;
+    position: fixed;
+    left: 0;
+    top: 10%;
+    height:100%;
+
+    z-index: -10;
+    background-image: url("<?php bloginfo('url');?>/wp-content/uploads/2015/03/step0.jpeg");
+    background-repeat: no-repeat;
+    transition: 0.4s linear;
+  }
+
+
+  .article {
+    padding: 50px;
+    font-size: 2em;
+    text-align: center;
+    font-family: 'Lora', serif;
+    color: black;
+    z-index: 100;
+  }
+
+  .stepheader {
+    font-size: 1.5em;
+    padding: 30px;
+  }
+
+  .step {
+    z-index: 100;
+    background: rgba(249, 247, 248, .6);
+  }
+    #credit{
+    font-size: .6em;
+    margin-bottom:20px;
+  }
+  </style>
+  <body>
+<div class="backbutton">
+   <a href="<?php bloginfo('url');?>"class="btn btn-success">Back to The Dartmouth</a>
+</div>
+
+<div class="intro" id="step0">
+<div class="header col-md-12">
+The Mirror: An Ideal Day in Hanover
+</div>
+<div class="blurb col-md-12">
+  <div id="credit">By Mary Liza Hartong and Andrew Kingsley
+<br>Digital edition by Robbie Neuhaus and Henry Wilson, with illustrations by Stephanie Ng</div>
+“Little town, it’s a quiet village. Every day like the one before” — lyrics from “Beauty and the Beast” (1991) or the everyday musings of students as they stroll about Hanover? For those of us without cars or the select few who were lucky enough to grow up in thriving metropolises, it’s no secret that this tiny New England town can often feel more like 18th-century rural France than a thriving mecca of young college students. (Without spontaneous singing, it can sometimes feel even duller.) For all its shortcomings, though, this town does have one advantage over the Parisian countryside — you don’t have to shack up with a hideous beast in order to explore the thrills it has to offer. With that in mind, this week we set out to enjoy an “ideal day” in Hanover. If you care to join us on our journey, we say, “Be our guest.”<br><br>
+▾ Scroll down ▾
+</div>
+</div>
+<div class="article container">
+<div id="imgholder"></div>
+<div class="step col-md-offset-4">
+<div class="stepheader" id="step1">First Stop: Lou’s</div>
+While this haven of hash browns, hollandaise and hot coffee hardly needs explaining to any student of the College, you may not know just how delightful the Hanover haunt is around noon on a weekday. There’s something delicious about fine dining when you know most of your peers are jailed in windowless classrooms, yawning their way through chapters and chapters of literary theory. There Mary Liza feasted on the most beautiful poppy seed muffin in existence, wolfed down the better half of a chicken pot pie and settled into the alluring green booth for a long winter’s nap. Whether you’re a trusty Saturday morning customer or prefer to attend for the occasional birthday breakfast, when you sit down at Lou’s you can’t deny the overwhelming feeling of familiarity there. Maybe it has something to do with that ever-present strawberry jam, the kind that might sit on your own kitchen counter at home, waiting to be spooned onto your dad’s morning toast. Perhaps it is the waitresses, who inexplicably bring that poppy seed muffin out before you can emit even the slightest frenzied word of hunger, all the while telling you about the last 18 years they’ve spent calling this little town home.
+<br><br>
+<div class="stepheader" id="step2">Dirt Cowboy</div>
+After departing Lou’s, we wandered a short distance across the way into Dirt Cowboy. Though not exactly secluded, this cozy coffee shop remains Hanover’s hidden jewel. So few students seem to know the wealth of wonders it offers that we felt like a modern-day Ponce de León, having discovered this fountain of the yummiest foods for ourselves. A window of gelato and chocolate bonbons greets you as you enter, dissolving into waves of coffee aromas which are mixed with the dulcet murmur of couples leaning over cappuccinos.  Whether rejuvenated by their vast coffee and tea selections or simply because we were lucky enough to relish the shimmering golden goodness of their apple Danish, we feel the need to bring the news to our people. Behold their chalices of orange, mango, pineapple smoothies, those Holy Grails of gastronomic delight. Savor their turkey sandwiches, slathered in their golden balsamic mustard. Dirt Cowboy is our El Dorado, yet they price their wares like they’re selling fool’s gold — so accessibly. So don’t be a fool, and discover for yourself this epicurean Eden. 
+<br><br>
+<div class="stepheader" id="step3">Folk</div>
+Likely known to most students as the dark, cramped store across from EBAs that carries the faint scent of incense even outside its window, FOLK is anyone’s one-stop shop for indie concert gear and graphic tees. Its nooks and crannies are filled with surprises, from Free People shirts and Nepalese jewelry to oriental pillowcases and motivational socks. Don’t go in with a J Crew mentality — this store is too chill for your intense corporate mojo. When Andrew went up to the counter to show an employee the items he was taking to the changing room, she didn’t hand him some number sign or ask him to mention her to the store manager. No, she just shrugged “okay,” mocking his Uniqlo energy. Enter the door, and let the bath of soothing exotic scents overcome your midterm malaise. The arabesques of color and hand-wrought crafts take you away from commercial retailers, where the jewelry is assembled by gears and switches, and connect you to the artisans, so that the delicacy of their fingers is felt in the dangle of an earring. Although pricey, the store sends off free Zen vibes to all customers. So stop in, be transported, chill.  
+<br><br>
+<div class="stepheader" id="step4">The Chocolate Shop</div>
+One of the hardest parts of being away from home is feeling removed from a community of mentors and adults who love you. Mary Liza certainly misses shooting the breeze with the dean of students at her high school, who, god bless her, tolerated Mary Liza’s egregious transgressions against the school uniform — à la various brooches and patterned knee socks. Instead of rolling her eyes at Mary Liza’s festive attire, the dean took the time to ask about Mary Liza’s day and recount her own with an open heart. We did not expect the same cordial welcome from the strangers working there when we entered The Chocolate Shop. We couldn’t have been more wrong, and we ended up having a 20-minute conversation with the owner about dogs — ours and hers, dogs we cherished and dogs we missed. By the time she rang up our goodies, we had forgotten we had entered the shop for chocolate alone. We told her we looked forward to meeting her new puppy in the coming weeks. To tell the truth, we also looked forward to meeting her again.
+<br><br>
+<div class="stepheader" id="step5">Bank of America</div>
+Sterile, utilitarian and barren, the lobby of Bank of America seems like the last place to experience the thrills of Hanover. As Andrew wearily deposited a check in the ATM while Mary Liza checked Yik Yak, though, in walked a miniature Australian shepherd named Walker. A patchwork of brown, black and white fur with opalescent milk blue eyes, Walker made this ordinary transaction a tribute to the intimacy of our hamlet. Whether an impromptu chat with the poster store owner or a belly rub for an adorable dog, the quaintness of the town brings together locals with stories and passions begging to be shared. You never know what surprises await you in Hanover — all you have to do is explore. 
+<br><br>
+<div class="stepheader" id="step6">Candela Tapas Lounge</div>
+Okay, so we didn’t exactly make it to Candelas, but we couldn’t resist including it as a feature on our platonic — if unrealized — ideal of a Hanover day. Across the street from the Dartmouth Entrepreneurial Network, Candela makes a meal out of Spanish appetizers. Dim, noisy and filled with chic, modern fare, Candela feels like an NYC restaurant that got lost and found itself in the rolling hills of New Hampshire. Their tiny plates of fish tacos, yucca fries and mushroom bites invite sharing, rendering it the ideal getaway for a romantic meal. Candela brings us back to our primitive past, where eating is communal, and our fingers become a fork, knife and spoon. Though your wallet will urge you to keep walking to nearby Ramunto’s, Candela is a perfect end-of-term celebration spot. You’ll emerge refreshed and unwilling to settle back into the tired Pine routine.
+<br><br>
+By the end of our day of revelry, we looked around Hanover with a smile, thinking, “There may be something there that wasn’t there before.”
+</div>
+</div>
+</div>
+  
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+    var currentStep = 0;
+    $(window).scroll(function () { 
+        // console.log("step top: " + $("#step1").position().top + ", scroll top: " + $(window).scrollTop() + ", height: " + window.innerHeight);
+        var oldStep = currentStep;
+        if ($(window).scrollTop() <= $("#step0").position().top && $(window).scrollTop() + window.innerHeight >= $("#step0").position().top) {
+            // if ($("#step1").position().top + $("#step1").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 0;
+          // }
+    }
+      if ($(window).scrollTop() <= $("#step1").position().top && $(window).scrollTop() + window.innerHeight >= $("#step1").position().top) {
+        if ($("#step1").position().top + $("#step1").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 1;
+        }
+    }
+      else if ($(window).scrollTop() <= $("#step2").position().top && $(window).scrollTop() + window.innerHeight >= $("#step2").position().top) {
+        if ($("#step2").position().top + $("#step2").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 2;
+        }
+    }
+      else if ($(window).scrollTop() <= $("#step3").position().top && $(window).scrollTop() + window.innerHeight >= $("#step3").position().top) {
+        if ($("#step3").position().top + $("#step3").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 3;
+        }
+    }
+      else if ($(window).scrollTop() <= $("#step4").position().top && $(window).scrollTop() + window.innerHeight >= $("#step4").position().top) {
+        if ($("#step4").position().top + $("#step4").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 4;
+        }
+    }
+      else if ($(window).scrollTop() <= $("#step5").position().top && $(window).scrollTop() + window.innerHeight >= $("#step5").position().top) {
+        if ($("#step5").position().top + $("#step5").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 5;
+        }
+    }
+    else if ($(window).scrollTop() <= $("#step6").position().top && $(window).scrollTop() + window.innerHeight >= $("#step6").position().top) {
+        if ($("#step6").position().top + $("#step6").height() <= $(window).scrollTop() + window.innerHeight) {
+            currentStep = 6;
+        }
+    }
+    if (oldStep != currentStep) {
+      var url = 'url("<?php bloginfo('url');?>/wp-content/uploads/2015/03/step' + currentStep + '.jpeg")';
+      $("#imgholder").css("background-image", url);
+    }
+});
+    </script>
+  </body>
