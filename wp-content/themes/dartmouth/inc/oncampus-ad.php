2f7584cf2b99c29221e5d8f21e0bdd25eefbb6f8
Jeremy <jeremy.c.mittleman.17@dartmouth.edu>
2016-04-08 12:25:02 -0400

    Update ad tag urls

--- wp-content/themes/dartmouth/inc/oncampus-ad.php
+++ wp-content/themes/dartmouth/inc/oncampus-ad.php
@@ -8,11 +8,11 @@
 			OX_ads.push({ "auid" : "<?php echo $auid; ?>" });
 		</script>
 		<script type="text/javascript">
-			document.write('<scr'+'ipt src="http://ox-d.oncampusweb.com/w/1.0/jstag"><\/scr'+'ipt>');
+			document.write('<scr'+'ipt src="//oncampusweb-d.openx.net/w/1.0/jstag"><\/scr'+'ipt>');
 		</script>
 		<noscript>
-			<iframe id="<?php echo $id; ?>" name="<?php echo $id; ?>" src="http://ox-d.oncampusweb.com/w/1.0/afr?auid=<?php echo $auid; ?>&cb=<?php echo $random_number; ?>" frameborder="0" scrolling="no" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
-				<a href="http://ox-d.oncampusweb.com/w/1.0/rc?cs=<?php echo $id; ?>&cb=<?php echo $random_number; ?>" ><img src="http://ox-d.oncampusweb.com/w/1.0/ai?auid=<?php echo $auid; ?>&cs=<?php echo $id; ?>&cb=<?php echo $random_number; ?>" border="0" alt=""></a>
+			<iframe id="<?php echo $id; ?>" name="<?php echo $id; ?>" src="//oncampusweb-d.openx.net/w/1.0/afr?auid=<?php echo $auid; ?>&cb=<?php echo $random_number; ?>" frameborder="0" scrolling="no" width="<?php echo $width; ?>" height="<?php echo $height; ?>">
+				<a href="//oncampusweb-d.openx.net/w/1.0/rc?cs=<?php echo $id; ?>&cb=<?php echo $random_number; ?>" ><img src="//oncampusweb-d.openx.net/w/1.0/ai?auid=<?php echo $auid; ?>&cs=<?php echo $id; ?>&cb=<?php echo $random_number; ?>" border="0" alt=""></a>
 			</iframe>
 		</noscript>
 	<?php
